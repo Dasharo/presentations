a49c198cb5b914fd8aa150d6d32e530b6f4336e9
Piotr Król <piotr.krol@3mdeb.com>
2023-12-05 00:40:15 +0100

    dasharo_issues.py: initial commit
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- dug_4/diagrams/dasharo_issues.py
+++ dug_4/diagrams/dasharo_issues.py
@@ -1,41 +1,90 @@
 #!/usr/bin/env python3
 
+import yaml
+import pytz
+import os
+from github import Github
+import datetime
 import matplotlib.pyplot as plt
+import requests
+from datetime import datetime
+import subprocess
 
-# Data
-quarters = ['Q1\'23', 'Q2\'23', 'Q3\'23', 'Q4\'23']
-total_issues = [360, 455, 484, 598]
-closed_issues = [125, 166, 195, 245]
-open_issues = [total - closed for total, closed in zip(total_issues, closed_issues)]
+def get_github_token():
+    # Path to the GitHub hosts.yml file
+    config_path = os.path.expanduser('~/.config/gh/hosts.yml')
 
-# Create the plot
+    # Read the YAML file
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Extract the OAuth token
+    return config['github.com']['oauth_token']
+
+def run_gh_command(command):
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def count_issues(repo, state, date):
+    # Construct the GitHub CLI command
+    command = f"gh issue list --repo {repo} --state {state} --search 'created:<={date}' -L 10000 --json number --jq '. | length'"
+    
+    # Run the command and capture the output
+    output = run_gh_command(command)
+
+    # Parse the output to get the count
+    if output is not None:
+        return int(output)
+    else:
+        return 0
+
+repo = "Dasharo/dasharo-issues"
+dates = ["2023-03-16", "2023-07-06", "2023-09-28", "2023-12-03"]
+
+# Data storage
+data = {"Total": [], "Closed": [], "Open": []}
+
+# Gather data
+for date in dates:
+    total_issues = count_issues(repo, 'all', date)
+    closed_issues = count_issues(repo, 'closed', date)
+    open_issues = count_issues(repo, 'open', date)
+    data["Total"].append(total_issues)
+    data["Closed"].append(closed_issues)
+    data["Open"].append(open_issues)
+
+# Plotting
 plt.figure(figsize=(10, 6))
 
 # Stacked bar chart
-bars_closed = plt.bar(quarters, closed_issues, color='#38d430', label='Closed Issues')
-bars_open = plt.bar(quarters, open_issues, bottom=closed_issues, color='#272727', label='Open Issues')
+bars_closed = plt.bar(dates, data["Closed"], color='#38d430', label='Closed issues')
+bars_open = plt.bar(dates, data["Open"], bottom=data["Closed"], color='#272727', label='Open issues')
 
 # Add titles and labels
-plt.title('Dasharo Issues in 2023', fontsize=16, fontweight='bold', color='#272727')
-plt.xlabel('Quarter', fontsize=14, fontweight='bold', color='#272727')
-plt.ylabel('Number of Issues', fontsize=14, fontweight='bold', color='#272727')
+plt.title('Dasharo Issues Statistics', fontsize=18, fontweight='bold', color='#272727')
+plt.xlabel('Date', fontsize=16, fontweight='bold', color='#272727')
+plt.ylabel('Number of Issues', fontsize=16, fontweight='bold', color='#272727')
 
 # Function to add labels inside the bars
-def add_labels(bars):
+def add_labels(bars, color='white'):
     for bar in bars:
         height = bar.get_height()
         plt.annotate(f'{height}',
                      xy=(bar.get_x() + bar.get_width() / 2, bar.get_y() + height / 2),
                      xytext=(0, 3),  # 3 points vertical offset
                      textcoords="offset points",
-                     ha='center', va='bottom', color='white', fontsize=10)
+                     ha='center', va='bottom', color=color, fontsize=14, fontweight='bold')
 
 # Add labels to the bars
 add_labels(bars_closed)
 add_labels(bars_open)
 
 # Add legend
-plt.legend()
+plt.legend(fontsize=12)
 
 # Set the background color
 plt.gca().set_facecolor('#f5f5f5')
@@ -45,4 +94,3 @@ plt.savefig('dasharo_issues.png')
 
 # Optionally, close the plot to free up memory
 plt.close()
-
