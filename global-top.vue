b18bb6900d035d2ef0df2640f27515cf32f9df40
Piotr Król <piotr.krol@3mdeb.com>
2024-09-09 14:18:13 +0200

    add 3mdeb theme, fonts and some mods
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

---
+++ global-top.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Scroll/>
+    <footer
+      v-if="$nav.currentLayout !== 'cover'"
+      class="absolute bottom-0 left-0 right-0 text-center py-2 text-[#000000]"
+    >
+      <small>© Copyright 2024. All Rights Reserved by 3mdeb Sp. z o.o.</small>
+      <div class="absolute bottom-0 right-0 py-2 pr-2 text-right text-[#000000]">
+        <small><SlideCurrentNo/>/<SlidesTotal/></small>
+      </div>
+    </footer>
+  </div>
+</template>
