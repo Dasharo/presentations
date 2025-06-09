b387f459754381a43f05c42be6c3b6ded1242934
Piotr Król <piotr.krol@3mdeb.com>
2025-06-10 00:28:37 +0200

    global-top.vue: use generic footer for all slides
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

---
+++ global-top.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Scroll />
+    <footer
+      v-if="$nav.currentLayout !== 'cover'"
+      class="absolute bottom-0 left-0 right-0 flex justify-between items-center py-2 text-[#000000]"
+    >
+      <small>© 2025 3mdeb Sp. z o.o. Licensed under the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0</a>. .
+</small>
+      <small class="mr-4">{{ $nav.currentPage }}</small>
+    </footer>
+  </div>
+</template>
