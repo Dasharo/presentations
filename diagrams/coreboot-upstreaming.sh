3a9286c9105222c76cd2506ad2498906c533556d
Filip Lewiński <filip.lewinski@3mdeb.com>
2025-12-08 09:05:21 +0100

    diagrams/coreboot-upstreaming.sh: satisfy pre-commit
    Signed-off-by: Filip Lewiński <filip.lewinski@3mdeb.com>

--- diagrams/coreboot-upstreaming.sh
+++ diagrams/coreboot-upstreaming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./contribution-stats sync -d dasharo
-./contribution-stats list -d dasharo -r coreboot -s 01/01/2000 -e $(date +"%m/%d/%Y") -o /tmp/coreboot_stat.csv
+./contribution-stats list -d dasharo -r coreboot -s 01/01/2000 -e "$(date +"%m/%d/%Y")" -o /tmp/coreboot_stat.csv
 
 echo "coreboot lines added:"
 awk -F';' '{sum += $6} END {print sum}' /tmp/coreboot_stat.csv
